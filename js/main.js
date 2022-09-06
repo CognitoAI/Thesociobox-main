9aebff5e156c5df121dd276c82f20da241970ab1
sony-shah <sonyshah248@gmail.com>
2022-09-06 14:18:03 +0530

    footer

--- js/main.js
+++ js/main.js
@@ -175,3 +175,51 @@ $(function () {
   });
 });
 
+
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+    // mybutton.css("opacity", "1");
+  } else {
+    mybutton.style.display = "none" ;
+    // mybutton.css("opacity", "0");
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  // document.body.scrollTop = 0;
+  // document.documentElement.scrollTop = 0;
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1000);
+    return false;
+}
+
+
+// start btn 
+$("#startbtn").click(function() {
+  $('html, body').animate({
+      scrollTop: $("#form-page").offset().top
+  }, 800);
+});
+
+
+// start btn 
+$("#campaign-btn").click(function() {
+  $('html, body').animate({
+      scrollTop: $("#form-page").offset().top
+  }, 800);
+});
+
+// start btn campaign
+$("#campaign-start").click(function() {
+  $('html, body').animate({
+      scrollTop: $("#form-page").offset().top
+  }, 800);
+});
